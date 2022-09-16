9a6cc2f62b05b14e9557063283c5dbbd513d8982
Vitalii Sichev <siche.vitalii.ru@gmail.com>
2022-09-16 18:19:58 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const exitHook = require("async-exit-hook");
+const config = require("./config");
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+
+const run = async () => {
+    await mongoose.connect(config.mongo.db, config.mongo.options);
+    app.listen(port, () => {
+        console.log(`Server started on ${port} port!`);
+    });
+
+    exitHook(() => {
+        mongoose.disconnect();
+        console.log("Mongoose disconnect");
+    });
+};
+
+run().catch(e => console.error(e));
