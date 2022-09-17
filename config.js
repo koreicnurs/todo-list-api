5833ad1f4da3ae528c4ea3f9832c075d9e2920bc
Vitalii Sichev <siche.vitalii.ru@gmail.com>
2022-09-17 16:20:34 +0600

    #1 Remove error file config

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
-const path = require("path");
 const rootPath = __dirname;
 
 module.exports = {
     rootPath,
-    uploadPath: path.join(rootPath, "public/uploads"),
     mongo: {
         db: "mongodb://localhost/todoList",
         options: {useNewUrlParser: true},
