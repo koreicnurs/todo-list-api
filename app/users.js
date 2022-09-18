68c2d4feb0d67ad825dd4eccaf9aa8c08e135b9e
koreicnurs <koreicnurs@gmail.com>
2022-09-18 16:18:35 +0600

    #3 Added login user by password and username and create new token

--- app/users.js
+++ app/users.js
@@ -35,4 +35,24 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.post('/sessions', async (req, res) => {
+    const user = await User.findOne({username: req.body.username});
+
+    if (!user) {
+        return res.status(401).send({error: 'User not found'});
+    }
+
+    const isMatch = await user.checkPassword(req.body.password);
+
+    if (!isMatch) {
+        res.status(401).send({error: 'Password is wrong'});
+    }
+
+    user.generateToken();
+    await user.save();
+
+    res.send({message: 'User and password correct!', user});
+
+});
+
 module.exports = router;
