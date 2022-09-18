8d2bcdde51e9ceef63b4fadb917b0c7ff910a0e2
Kylymbek Shaihidinov <shaihidinovkylymbek@gmail.com>
2022-09-18 16:37:29 +0600

    #5 Added GET method to tasks

--- app/tasks.js
+++ app/tasks.js
@@ -5,19 +5,25 @@ const Task = require('../models/Task');
 const auth = require('../middleware/auth');
 
 router.post('/', auth, async (req, res) => {
-    const {title,description,status} = req.body;
+    const {title, description, status} = req.body;
     const user = req.user._id;
 
     if (!title || !description || !status) {
         res.status(400).send({error: 'Data not valid'});
     }
 
-    const taskData = {user,title,description, status};
+    const taskData = {user, title, description, status};
     const task = new Task(taskData);
     await task.save();
 
-    res.send({message:'Task successfully saved',task});
+    res.send({message: 'Task successfully saved', task});
 });
 
+router.get('/', auth, async (req, res) => {
+
+    const tasks = await Task.find({user: req.user._id});
+
+    res.send(tasks);
+});
 
 module.exports = router;
